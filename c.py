dc5625ee236ce6807bda576faf9ed10c6f0728a4
Ken <kenkilmartin@gmail.com>
2018-04-06 02:12:08 +0100

    finnished project with report

--- c.py
+++ c.py
@@ -1,6 +1,7 @@
 
 # Echo client program
 import socket
+import threading, Queue
 import hashlib
 import time
 
@@ -8,36 +9,39 @@ HOST = '127.0.0.1'    # The remote host
 PORT = 50007          # The same port as used by the server
 
 
+print "What is your user name:"
+username = raw_input()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
+s.send("<join>" + username + "</join>")
+
+# ==
 print "type input:"
-
-#if ping in text
-#then start the timer
 text = raw_input()
+
 if "ping" in text:
-    #start the timer
-    startTime = time.time()
-    print "ping start time" + str(startTime)
-	
-	
+	start_time = time.time()
+	print "ping started ->" + str(start_time)
 
-# raw structure for cmd example.. to build our own protocol
-# <cmd>message:hey - kdjskdfj342</cmd>
 
-hash= hashlib.sha224(text).hexdigest()
-
-output = '<cmd>message:'+text+'-'+hash+'</cmd>' #holder for the command - what we fire back to the server
-print output
 
 # when we send data to the server, we are using a colon
 # at the end of a sentence to mark the end of the current sentence
 # later when the input comes back, we will then be breaking the input
 # into individual parts using the colon : to separate the lines
 
+hash = hashlib.sha224(text).hexdigest()
+output = '<cmd>message:'+ text + '_' + hash + '</cmd><user>' + username + '</user>'# command
+
 if "ping" in text:
-    output = "<cmd>ping</cmd>"
+	output =  "<cmd>ping</cmd>"
+
+if "total" in text:
+	output =  "<cmd>total</cmd>"
+
+if "gettime" in text:
+	output =  "<cmd>gettime</cmd>"
 
 s.sendall(output + ":")
 
@@ -48,12 +52,14 @@ response = data.split(':')
 
 for x in response:
     print "Response:" + str(x)
-	
-if "pong" in response:
-    print "stop timer"
-    stopTime= time.time()
-    print "stop time " + str (stopTime)
-    totalTime = stopTime - startTime
-    print "total time " + str(totalTime)
+
+if "ping pong" in response:
+	stop_time = time.time()
+	trip_time = stop_time-start_time
+	print "stop time " + str(stop_time)
+	print str(trip_time) + " secs to reply"
     
 s.close()
+
+
+
--- s.py
+++ s.py
@@ -1,8 +1,8 @@
 import socket
 import threading, Queue
 import hashlib
-
-
+import time
+from time import gmtime, strftime
 
 HOST = '127.0.0.1'        
 PORT = 50007              
@@ -17,34 +17,50 @@ s.bind((HOST, PORT))
 # into the buffer string to be relayed later
 # to different clients that have connected
 # Each message in the buffer is separated by a colon :
-buffer = ""   
+buffer = ""  
+
+def parseUsername(data):
+    tempUser = data[6:len(data)]
+    openingBracketPos = tempUser.index('<')
+    name = tempUser[0: openingBracketPos]
+    return name 
 
 # custom say hello command
 def sayHello():
     print "----> The hello function was called"
-	
-def parseMessage(command):
-    print "parsing message..."
-	#removing the word "message:" (8 chars)
-    keyvaluePair = command[8:len(command)]
-	
-    print "full string: "+ str(keyvaluePair)
-    
-    dashPosition = keyvaluePair.index('-')
-    hash = keyvaluePair[dashPosition+1:len(keyvaluePair)]
-    
-    message = keyvaluePair[0:dashPosition]
-    print "message is: "+ message
-    newhash = hashlib.sha224(message).hexdigest()
-    print "Data rec hashed: "+str(newhash)
-    print "the hash is... "+str(hash)
-    
-  
-  
-def pong():
-    print "sending pong"
-    conn.send("pong")
 
+def getTime():
+    conn.send("Current time is " + strftime("%a, %d %b %Y %H.%M.%S", gmtime()))
+
+def parseMessage(command):
+    print 'parsing message...'
+    keyvaluePair = command[8: len(command)]
+
+    dashPosition = keyvaluePair.index('_')
+    hashed = keyvaluePair[dashPosition + 1:len(keyvaluePair)]
+
+    message = keyvaluePair[0: dashPosition]
+    hashedMessage = hashlib.sha224(message).hexdigest()
+   
+    if(hashedMessage in hashed):
+        return message
+    else:
+        print 'hashes do not match'
+
+def pong():
+    print "sending pong..."
+    start_time = time.time()
+    conn.send("ping pong")
+
+def count():
+    buffer_string = str(buffer)
+    stringArray = buffer_string.split("</cmd>")
+
+    if len(stringArray) <= 0:
+        conn.send("Number of messages -> 0")
+    else:
+        conn.send("Number of messages -> " + str(len(stringArray) - 1))
+   
 # sample parser function. The job of this function is to take some input
 # data and search to see if a command is present in the text. If it finds a 
 # command it will then need to extract the command.
@@ -61,19 +77,28 @@ def parseInput(data):
         # Add 5 on for the length of the <cmd>
         start = start + 5
         # chop up remving start and end. 
-        command = data[5:-7] #-7 chops of the end of the tag </cmd>
+        command = data[start:-6] #-7 chops of the end of the tag </cmd>
         
+
         # Once we find a command, we will then check if a specific command
         # is inside, if we find the word "hello" we are telling the server
         # to call the sayHello() function.
         if "hello" in command:
             sayHello()
-        elif "message" in command:
-		    parseMessage(command)
-        elif "ping" in command:
-		    pong()
+
+        if "message" in command:
+            message = parseMessage(command)
+            return message
         
-    
+        if "ping" in command:
+            pong()
+
+        if "total" in command:
+            count()
+        if "gettime" in command:
+            getTime()
+
+        
 # we a new thread is started from an incoming connection
 # the manageConnection funnction is used to take the input
 # and print it out on the server
@@ -81,24 +106,37 @@ def parseInput(data):
     
 def manageConnection(conn, addr):
     global buffer
-    print 'Connected by', addr
-    
-    
+   # print 'Connected by', addr
     data = conn.recv(1024)
+       
+    name = ''
+      
+    if '<user>' in data:
+        start_name = data.index('<user>')
+        name = data[start_name + 6: -8]
+        data = data[0:start_name]
     
-    parseInput(data)# Calling the parser
+    message = parseInput(data)# Calling the parser
     
-    print "rec:" + str(data)
-    buffer += str(data)
+    if message != '':
+        print "rec:" + str(data)
+        message = name + " says > " + message
+        buffer += str(message)
     
     conn.send(str(buffer))
-        
     conn.close()
 
 
 while 1:
     s.listen(1)
     conn, addr = s.accept()
+    data = conn.recv(1024)
+    print data;
+
+    if "<join>" in data:
+        username = parseUsername(data)
+        buffer += str(username + " has joined the conversation :")
+      
     # after we have listened and accepted a connection coming in,
     # we will then create a thread for that incoming connection.
     # this will prevent us from blocking the listening process
